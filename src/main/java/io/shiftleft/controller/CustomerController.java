c641950d36edb751b180b000724854240bb67256
Curtis Yanko <cmyanko@gmail.com>
2021-01-19 13:25:30 -0500

    Update CustomerController.java

--- src/main/java/io/shiftleft/controller/CustomerController.java
+++ src/main/java/io/shiftleft/controller/CustomerController.java
@@ -123,6 +123,7 @@ public class CustomerController {
 	  }
 
 	  Account account = new Account(4242l,1234, "savings", 1, 0);
+	  // TODO: cheap trace for debugging, be sure to remove!
 	  log.info("Account Data is {}", account);
 	  log.info("Customer Data is {}", customer);
 
