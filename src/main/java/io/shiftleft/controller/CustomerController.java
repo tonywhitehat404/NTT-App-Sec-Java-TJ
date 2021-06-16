c70e13a9cfc59171f1a99800dac07b365b34088c
Curtis Yanko <cmyanko@gmail.com>
2021-06-16 08:35:48 -0400

    Update CustomerController.java

--- src/main/java/io/shiftleft/controller/CustomerController.java
+++ src/main/java/io/shiftleft/controller/CustomerController.java
@@ -125,7 +125,7 @@ public class CustomerController {
 	  Account account = new Account(4242l,1234, "savings", 1, 0);
 	  // TODO: cheap trace for debugging, be sure to REMOVE!
 	  //log.info("Account Data is {}", account);
-	  //log.info("Customer Data is {}", customer);
+	  log.info("Customer Data is {}", customer);
 
       try {
         dispatchEventToSalesForce(String.format(" Customer %s Logged into SalesForce", customer));
